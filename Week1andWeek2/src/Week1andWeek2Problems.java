98ff767e4028a6030619cdb0a20ac3b495a8091e
Sai Venkat <saivenkatramachandra@gmail.com>
2026-03-16 16:05:28 +0530

    Update Week1andWeek2Problems.java

--- Week1andWeek2/src/Week1andWeek2Problems.java
+++ Week1andWeek2/src/Week1andWeek2Problems.java
@@ -112,7 +112,7 @@ class ParkingLot {
     }
 }
 
-public class Week1_and_Week2_Problems {
+public class Week1andWeek2Problems {
 
     public static void main(String[] args) throws InterruptedException {
 
